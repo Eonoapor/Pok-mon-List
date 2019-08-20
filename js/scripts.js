5d01823c615e47274986170e65040571799ba47f
Eonoapor <emmanuel.onoapor@gmail.com>
2019-08-20 13:32:25 -0700

    Pokémon objects
    Creation of new variable called repository and adding several objects to the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-alert('Hello world');
-var favoriteFood='Fried rice';
-document.write(favoriteFood);
+var repository = {
+    name: "Bulbasaur",       //The first Pokemon
+    height: 7,
+    type:['grass', 'poison'],
+    weight:6.9,
+pokemon2: {             //The second Pokemon
+    name:"Electrode",
+    height:1.2,
+    type:'electric',
+    weight:66.6,
+pokemon3:{              //The third Pokemon
+    name:"Machoke",
+    height:1.5,
+    type:'fighting',
+    weight:70.5,
+  }
+
+};
