427569d54bab1a88fba1492f04f78db6a46f61c5
Eonoapor <emmanuel.onoapor@gmail.com>
2019-08-25 21:50:39 -0700

    update
    update to mini app

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
 var repository = [
-    {name: 'Bulbasaur',       //The first Pokemon
-    height: 7,
+  {
+    name: 'Bulbasaur',       //The first Pokemon
+    height: 0.7,
     type:['grass', 'poison'],
-    weight:6.9},
-    {name:'Electrode',       //The second Pokemon
-    height:1.2,
-    type:'electric',
-    weight:66.6},
-    {name:'Machoke',         //The third Pokemon
-    height:1.5,
-    type:'fighting',
-    weight:70.5}
+    weight:6.9
+  },
+    {
+    name:'Electrode',       //The second Pokemon
+    height:1.7,
+    type:['electric'],
+    weight:7.6
+  },
+    {
+    name:'Machoke',         //The third Pokemon
+    height:0.5,
+    type: ['fighting'],
+    weight:6.5
+    }
   ];
+for (var i=0; i< repository.length; i++){
+  document.write( "Name: " + repository.name + " Height: " + repository.height + "Type: " + repository.type + "Weight: " + repository.weight);
+}
+  if (repository.height === 1.7) {
+    document.write( "Electrode (height:1.7)- Wow, that's big ");
+  }else if (repository.height === 0){
+    document.write( 'Normal size' );
+  }
