ca43166181ddbf2be947a713b4899191f4f63a84
Eonoapor <emmanuel.onoapor@gmail.com>
2019-08-26 21:25:08 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,29 @@
 var repository = [
   {
-    name: 'Bulbasaur',       //The first Pokemon
-    height: 0.7,
-    type:['grass', 'poison'],
-    weight:6.9
+      name: 'Bulbasaur',       //The first Pokemon
+      height: 0.7,
+      type:['grass', 'poison'],
+      weight:6.9
   },
-    {
-    name:'Electrode',       //The second Pokemon
-    height:1.7,
-    type:['electric'],
-    weight:7.6
+  {
+      name:'Electrode',       //The second Pokemon
+      height:1.7 ,
+      type:['electric'],
+      weight:7.6
   },
-    {
-    name:'Machoke',         //The third Pokemon
-    height:0.5,
-    type: ['fighting'],
-    weight:6.5
-    }
-  ];
-for (var i=0; i< repository.length; i++){
-  document.write( "Name: " + repository.name + " Height: " + repository.height + "Type: " + repository.type + "Weight: " + repository.weight);
-}
-  if (repository.height === 1.7) {
-    document.write( "Electrode (height:1.7)- Wow, that's big ");
-  }else if (repository.height === 0){
-    document.write( 'Normal size' );
+  {
+      name:'Machoke',         //The third Pokemon
+      height:0.5,
+      type: ['fighting'],
+      weight:6.5
   }
+  ]
+repository.forEach (function(repository) {
+    document.write(repository.name + "(" )
+    document.write("Height:" + repository.height +",")
+    document.write("Type:" + repository.type + ",")
+    document.write("Weight:" + repository.weight + ")" + "<br><br>")
+    if (repository ===1.7) {
+      document.write("Wow, that's big!")
+    }
+});
