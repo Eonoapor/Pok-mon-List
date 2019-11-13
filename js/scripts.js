c10821a9f976b6ab9e9775626c319916b06c4b64
E.Onoapor <emmanuel.onoapor@gmail.com>
2019-11-12 17:44:32 -0800

    Using Ajax to send and fetch data asynchronously from an external API
    Array of Pokémon objects was replaced with an empty array.
    
    Function(s) inside data repository function was sadded for loading data from an external source.
    
    Pokémon list is endered after getting all information from the server.
    
    showDetails() function was edited to load the Pokémon details from the API.
    
    Promise and Fetch polyfills added to project and linked in the index.html file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,13 @@
 
 var $pokemonList = document.querySelector('ul');
 
-var pokemonRepository = (function ()
-    {
-    var repository = [
-    {
-      name: 'Bulbasaur',       //The first Pokemon
-      height: 0.7,
-      type: ['grass', 'poison'],
-      weight: 6.9
-  },
-  {
-      name: 'Electrode',       //The second Pokemon
-      height: 1.7 ,
-      type: ['electric'],
-      weight: 7.6
-  },
-  {
-      name: 'Machoke',         //The third Pokemon
-      height: 0.5,
-      type: ['fighting'],
-      weight: 6.5
-  }
-];
+var pokemonRepository = (function(){
+  var repository = [];
+  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  //Array removed and replaced with external data source
 
 function add(pokemon){
+  function add(pokemon){
 
     if (typeof pokemon !== 'object'){
       return 'Invalid entry'
@@ -52,6 +35,34 @@ function add(pokemon){
 
   function showDetails(pokemon){
     console.log(pokemon)
+    pokemonRepository.loadDetails(pokemon).then(function(){//to get Pokémon details using URL from Pokémon object in parameter
+    });
+  function loadList(){ //Fuction as a return key
+    return fetch(apiUrl).then(function(response){ //fetch for complete list of pokemon
+      return response.json();
+    }).then(function(json){ //returning a JSON response
+      json.results.forEach(function(pokemon){
+        var pokemon = {
+          name: pokemon.name, //key
+          detailsUrl: pokemon.url //key
+        };
+        add(pokemon); // for adding Pokémon from results to repository variable
+      });
+    }).catch(function(error){
+      console.error(error);
+    })
+  }
+  function loadDetails(pokemon){
+    var url = pokemon.detailsUrl;
+    return fetch(url).then(function(response){
+      return response.json();
+    }).then(function(details){ //Assigning some details from response to object in repository
+      pokemon.imageUrl = details.sprites.front_default;
+      pokemon.height = details.height;
+      pokemon.types = Object.keys(details.types);
+    }).catch(function(error){
+      console.error(error);
+    })
   }
 
   return{
@@ -59,10 +70,18 @@ function add(pokemon){
     getAll: getAll,
     addListItem: addListItem,
     showDetails: showDetails
+    //showDetails: showDetails,
+    loadList: loadList,
+    loadDetails: loadDetails
   };
 })();
 
+pokemonRepository.loadList().then(function() { //Calling loadList function
 
 pokemonRepository.getAll().forEach(function(currentItem){
   pokemonRepository.addListItem(currentItem);
 })
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon); //calling the addListItem function
+  });
+});
